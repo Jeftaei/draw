0995cd39356bc16110609b406b45f4439c58f73e
Jeft <67933106+Jeftaei@users.noreply.github.com>
2024-05-19 21:26:12 -0500

    refactor/reorganizing of functions + circular drawing now

--- src/modules/canvas.rs
+++ src/modules/canvas.rs
@@ -70,22 +70,33 @@ impl Canvas {
         Ok(())
     }
 
-    pub fn to_pixel_pos(location: PhysicalPosition<f64>, size: PhysicalSize<u32>) -> u32 {
-        let x = (location.x as u32).clamp(0, size.width - 1);
-        let y = (location.y as u32).clamp(0, size.height - 1);
+    pub fn to_pixel_pos(&self, location: PhysicalPosition<f64>) -> u32 {
+        let x = (location.x as u32).clamp(0, self.canvas_size.width - 1);
+        let y = (location.y as u32).clamp(0, self.canvas_size.height - 1);
 
-        x + (y * size.width)
+        x + (y * self.canvas_size.width)
     }
 
-    pub fn get_pixels_in_line(
-        loc1: PhysicalPosition<f64>,
-        loc2: PhysicalPosition<f64>,
-        size: PhysicalSize<u32>,
-    ) -> Vec<u32> {
-        let mut points: Vec<u32> = vec![];
+    pub fn bulk_pixel_convert(&self, v: Vec<(i32, i32)>) -> Vec<u32> {
+        v.iter()
+            .map(|(x, y)| {
+                let x = (*x as u32).clamp(0, self.canvas_size.width - 1);
+                let y = (*y as u32).clamp(0, self.canvas_size.height - 1);
 
-        let (x1, y1) = (loc1.x as i32, loc1.y as i32);
-        let (x2, y2) = (loc2.x as i32, loc2.y as i32);
+                x + (y * self.canvas_size.width)
+            })
+            .collect::<Vec<u32>>()
+    }
+
+    pub fn get_line_points(
+        &self,
+        prev: PhysicalPosition<f64>,
+        curr: PhysicalPosition<f64>,
+    ) -> Vec<(i32, i32)> {
+        let mut points: Vec<(i32, i32)> = vec![];
+
+        let (x1, y1) = (prev.x as i32, prev.y as i32);
+        let (x2, y2) = (curr.x as i32, curr.y as i32);
 
         let dx = (x2 - x1).abs();
         let dy = (y2 - y1).abs();
@@ -97,7 +108,7 @@ impl Canvas {
         let mut y = y1;
 
         loop {
-            points.push(Self::to_pixel_pos((x, y).into(), size));
+            points.push((x, y));
 
             if x == x2 && y == y2 {
                 break;
@@ -117,31 +128,32 @@ impl Canvas {
         points
     }
 
-    pub fn get_cell_neighbors(
-        location: PhysicalPosition<f64>,
-        canvas_size: PhysicalSize<u32>,
-        brush_size: u32,
-    ) -> Vec<u32> {
-        // this code sucks, also not really want i want tbh
+    pub fn get_circle_points(&self, location: PhysicalPosition<f64>) -> Vec<(i32, i32)> {
+        let radius = self.brush_size as i32 - 1;
+        let mut points = Vec::<(i32, i32)>::new();
 
-        // let mut neighbors = vec![];
+        let x_center = location.x as i32;
+        let y_center = location.y as i32;
 
-        // let (x, y) = (location.x as u32, location.y as u32);
-        // let (x_bound, y_bound) = (canvas_size.width - 1, canvas_size.height - 1);
+        // i still want to draw 1 pixel sometimes
+        if self.brush_size == 1 {
+            return vec![(x_center, y_center)];
+        };
 
-        // for dx in (x.saturating_sub(brush_size))..=(x.saturating_add(brush_size)) {
-        //     for dy in (y.saturating_sub(brush_size))..=(y.saturating_add(brush_size)) {
-        //         if dx > x_bound || dy > y_bound {
-        //             continue;
-        //         }
-        //         // dbg!(dx, dy, x_bound, y_bound, x, y);
+        // Thanks stackoverflow, i dont like math.
+        // Very Bad "Algorithm"
+        for y in -radius..=radius {
+            for x in -radius..=radius {
+                if x * x + y * y < radius * radius + radius {
+                    points.push((x_center + x, y_center + y));
+                }
+            }
+        }
 
-        //         neighbors.push(Self::pixel_from_coord(dx, dy, canvas_size.width));
-        //     }
-        // }
-
-        // neighbors
-        vec![Self::to_pixel_pos(location, canvas_size)]
+        // sort vec and then call dedup, which removes all consecutive repeating values :D
+        points.sort();
+        points.dedup();
+        points
     }
 
     pub fn draw(
@@ -149,12 +161,16 @@ impl Canvas {
         location: PhysicalPosition<f64>,
         prev_location: PhysicalPosition<f64>,
     ) -> Result<(), Box<dyn Error>> {
+        let line_points = self.get_line_points(prev_location, location);
+
+        let mut points: Vec<u32> = Vec::new();
+
+        for p in line_points {
+            points.extend(self.bulk_pixel_convert(self.get_circle_points(p.into())));
+        }
+
         let mut buffer = self.surface.buffer_mut()?;
-
-        // let pxls: Vec<u32> = Self::get_cell_neighbors(location, self.canvas_size, self.brush_size);
-        let pxls: Vec<u32> = Self::get_pixels_in_line(prev_location, location, self.canvas_size);
-
-        for px in pxls {
+        for px in points {
             // dbg!(px);
             buffer[px as usize] = self.brush_color;
         }
